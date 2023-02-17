adb0611a68c260955b0073e325816dac77ccdabc
cydaw6 <56827368+cydaw6@users.noreply.github.com>
2023-02-17 11:05:56 +0100

    5 files modified

--- src/todo-list/todo-item/todo-item.component.ts
+++ src/todo-list/todo-item/todo-item.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css'],
+  imports: [CommonModule],
   standalone: true,
 })
 export class TodoItemComponent implements OnInit {
